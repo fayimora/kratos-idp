037bf8fd3a11ae25e4ef57960137daa4bd2c329e
Fayi <fayi@fayimora.com>
2024-08-19 12:46:28 +0100

    add ability to logout

--- kratos-frontend/src/routes/__root.tsx
+++ kratos-frontend/src/routes/__root.tsx
@@ -1,16 +1,50 @@
+import { kratos } from "@/lib/utils";
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { useEffect, useState } from "react";
+import { Session } from "@ory/client";
 
 export const Route = createRootRoute({
-  component: () => (
+  component: () => <Root />,
+});
+
+function Root() {
+  const [logoutUrl, setLogoutUrl] = useState<string>();
+  const [session, setSession] = useState<Session | null>(null);
+
+  const createLogoutFlow = async () => {
+    try {
+      const { data: flow } = await kratos.createBrowserLogoutFlow();
+      console.log("logout flow", flow);
+      setLogoutUrl(flow.logout_url);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    kratos.toSession().then(({ data: session }) => {
+      setSession(session);
+      createLogoutFlow();
+    });
+  }, []);
+
+  return (
     <>
       <div className="p-2 flex gap-2">
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>{" "}
-        <Link to="/login" className="[&.active]:font-bold">
-          Sign In
-        </Link>
+        {session ? (
+          <Link to={logoutUrl} replace={true} className="[&.active]:font-bold">
+            {" "}
+            Sign Out{" "}
+          </Link>
+        ) : (
+          <Link to="/login" className="[&.active]:font-bold">
+            Sign In
+          </Link>
+        )}
         <Link to="/register" className="[&.active]:font-bold">
           Sign Up
         </Link>
@@ -25,5 +59,5 @@ export const Route = createRootRoute({
       </div>
       <TanStackRouterDevtools />
     </>
-  ),
-});
+  );
+}
